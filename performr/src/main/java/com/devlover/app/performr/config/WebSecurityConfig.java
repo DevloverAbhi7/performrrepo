6eb54f9be590b7521b8421fc69077978759ec644
send2 <abhinavgr007@hotmail.com>
2019-11-17 03:24:50 +0530

    user registeration added

--- performr/src/main/java/com/devlover/app/performr/config/WebSecurityConfig.java
+++ performr/src/main/java/com/devlover/app/performr/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/css/*","/fonts/*","/images/*","/js/*","/vendor/*","/index","/authenticate", "/register","/generateotp","/*").permitAll().
+				.authorizeRequests().antMatchers("/registeruser","/css/*","/fonts/*","/images/*","/js/*","/vendor/*","/index","/authenticate", "/register","/generateotp","/*").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
--- performr/src/main/java/com/devlover/app/performr/controller/IndexController.java
+++ performr/src/main/java/com/devlover/app/performr/controller/IndexController.java
@@ -1,8 +1,27 @@
 package com.devlover.app.performr.controller;
 
+import java.io.IOException;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.devlover.app.performr.model.CommonResponse;
+import com.devlover.app.performr.model.Users;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @Controller
 public class IndexController 
@@ -19,4 +38,84 @@ public class IndexController
 		return "register";
 	}
 	
+	@PostMapping("registeruser")
+	public String registerUser(@ModelAttribute User usr ) throws IOException
+	{
+		final String uri = "http://localhost:8080/register";
+		OkHttpClient client = new OkHttpClient();
+		ObjectMapper mapper = new ObjectMapper();
+		MediaType mediaType = MediaType.parse("application/json");
+		RequestBody body = RequestBody.create(mediaType, mapper.writeValueAsString(usr));
+		Request request = new Request.Builder()
+		  .url("http://performrweb.azurewebsites.net:8080/register")
+		  .post(body)
+		  .addHeader("Content-Type", "application/json")
+		  .addHeader("Accept", "*/*")
+		  .addHeader("Cache-Control", "no-cache")
+		  .addHeader("Host", "localhost:8080")
+		  .addHeader("Accept-Encoding", "gzip, deflate")
+		  .addHeader("Content-Length", "100")
+		  .addHeader("Connection", "keep-alive")
+		  .addHeader("cache-control", "no-cache")
+		  .build();
+
+		Response response = client.newCall(request).execute();
+		CommonResponse user = mapper.readValue(response.body().string(), CommonResponse.class);
+		
+		if(user.getCode().equals("200"))
+		{
+			return "success";
+		}
+		else 
+		{
+			return "failed";
+		}
+	}
+	
+	static class User
+	{
+		
+		private String firstname;
+		
+	
+		private String lastname;
+		
+		
+		
+		private String username;
+		
+		private String password;
+
+		public String getFirstname() {
+			return firstname;
+		}
+
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
+
+		public String getLastname() {
+			return lastname;
+		}
+
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	}
 }
